cfa6e52b1b805162a35f7401466065f942114ff7
TANISH <77963254+G8ur@users.noreply.github.com>
2024-12-13 20:42:13 +0530

    donw with vite

---
+++ client/vite.config.js
@@ -0,0 +1,29 @@
+// import { defineConfig } from "vite";
+// import react from "@vitejs/plugin-react";
+
+// export default defineConfig({
+//     plugins:[react()],
+//     server:{
+//      port:3000,
+//      open:true
+//     }
+
+
+// });
+
+import path from "path"
+import react from "@vitejs/plugin-react"
+import { defineConfig } from "vite"
+
+export default defineConfig({
+  plugins: [react()],
+  server:{
+    port:3000,
+    open:true
+  },
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+})
