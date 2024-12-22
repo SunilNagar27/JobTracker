5d81068455a7cfec72be5cb5f24ce84c7c912a88
TANISH <77963254+G8ur@users.noreply.github.com>
2024-12-22 10:00:36 +0530

    done with contact us page

--- client/src/pages/contact.jsx
+++ client/src/pages/contact.jsx
@@ -1,148 +1,188 @@
-import emailjs from "@emailjs/browser";
-import { Suspense, useRef, useState } from "react";
+// import emailjs from "@emailjs/browser";
+// import { Suspense, useRef, useState } from "react";
 
 
-import useAlert from "@/hooks/useAlert";
-import Alert from "@/components/Alert";
-const Contact = () => {
-  const formRef = useRef();
-  const [form, setForm] = useState({ name: "", email: "", message: "" });
-  const { alert, showAlert, hideAlert } = useAlert();
-  const [loading, setLoading] = useState(false);
-  const [currentAnimation, setCurrentAnimation] = useState("idle");
+// import useAlert from "@/hooks/useAlert";
+// import Alert from "@/components/Alert";
+// const Contact = () => {
+//   const formRef = useRef();
+//   const [form, setForm] = useState({ name: "", email: "", message: "" });
+//   const { alert, showAlert, hideAlert } = useAlert();
+//   const [loading, setLoading] = useState(false);
+//   const [currentAnimation, setCurrentAnimation] = useState("idle");
 
-  const handleChange = ({ target: { name, value } }) => {
-    setForm({ ...form, [name]: value });
-  };
+//   const handleChange = ({ target: { name, value } }) => {
+//     setForm({ ...form, [name]: value });
+//   };
 
-  const handleFocus = () => setCurrentAnimation("walk");
-  const handleBlur = () => setCurrentAnimation("idle");
+//   const handleFocus = () => setCurrentAnimation("walk");
+//   const handleBlur = () => setCurrentAnimation("idle");
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    setLoading(true);
-    setCurrentAnimation("hit");
+//   const handleSubmit = (e) => {
+//     e.preventDefault();
+//     setLoading(true);
+//     setCurrentAnimation("hit");
 
-    emailjs
-      .send(
-        import.meta.env.VITE_APP_EMAILJS_SERVICE_ID,
-        import.meta.env.VITE_APP_EMAILJS_TEMPLATE_ID,
-        {
-          from_name: form.name,
-          to_name: "JavaScript Mastery",
-          from_email: form.email,
-          to_email: "sujata@jsmastery.pro",
-          message: form.message,
-        },
-        import.meta.env.VITE_APP_EMAILJS_PUBLIC_KEY
-      )
-      .then(
-        () => {
-          setLoading(false);
-          showAlert({
-            show: true,
-            text: "Thank you for your message 😃",
-            type: "success",
-          });
+//     emailjs
+//       .send(
+//         import.meta.env.VITE_APP_EMAILJS_SERVICE_ID,
+//         import.meta.env.VITE_APP_EMAILJS_TEMPLATE_ID,
+//         {
+//           from_name: form.name,
+//           to_name: "JavaScript Mastery",
+//           from_email: form.email,
+//           to_email: "sujata@jsmastery.pro",
+//           message: form.message,
+//         },
+//         import.meta.env.VITE_APP_EMAILJS_PUBLIC_KEY
+//       )
+//       .then(
+//         () => {
+//           setLoading(false);
+//           showAlert({
+//             show: true,
+//             text: "Thank you for your message 😃",
+//             type: "success",
+//           });
 
-          setTimeout(() => {
-            hideAlert(false);
-            setCurrentAnimation("idle");
-            setForm({
-              name: "",
-              email: "",
-              message: "",
-            });
-          }, [3000]);
-        },
-        (error) => {
-          setLoading(false);
-          console.error(error);
-          setCurrentAnimation("idle");
+//           setTimeout(() => {
+//             hideAlert(false);
+//             setCurrentAnimation("idle");
+//             setForm({
+//               name: "",
+//               email: "",
+//               message: "",
+//             });
+//           }, [3000]);
+//         },
+//         (error) => {
+//           setLoading(false);
+//           console.error(error);
+//           setCurrentAnimation("idle");
 
-          showAlert({
-            show: true,
-            text: "I didn't receive your message 😢",
-            type: "danger",
-          });
-        }
-      );
-  };
+//           showAlert({
+//             show: true,
+//             text: "I didn't receive your message 😢",
+//             type: "danger",
+//           });
+//         }
+//       );
+//   };
 
-  return (
-    <>
-    <section className='relative flex lg:flex-row flex-col max-container'>
-      {alert.show && <Alert {...alert} />}
+//   return (
+//     <>
+//     <section className='relative flex lg:flex-row flex-col max-container'>
+//       {alert.show && <Alert {...alert} />}
 
-      <div className='flex-1 min-w-[50%] flex flex-col'>
-        <h1 className='head-text'>Get in Touch</h1>
+//       <div className='flex-1 min-w-[50%] flex flex-col'>
+//         <h1 className='head-text'>Get in Touch</h1>
 
-        <form
-          ref={formRef}
-          onSubmit={handleSubmit}
-          className='w-full flex flex-col gap-7 mt-14'
-        >
-          <label className='text-black-500 font-semibold'>
-            Name
-            <input
-              type='text'
-              name='name'
-              className='input'
-              placeholder='John'
-              required
-              value={form.name}
-              onChange={handleChange}
-              onFocus={handleFocus}
-              onBlur={handleBlur}
-            />
-          </label>
-          <label className='text-black-500 font-semibold'>
-            Email
-            <input
-              type='email'
-              name='email'
-              className='input'
-              placeholder='John@gmail.com'
-              required
-              value={form.email}
-              onChange={handleChange}
-              onFocus={handleFocus}
-              onBlur={handleBlur}
-            />
-          </label>
-          <label className='text-black-500 font-semibold'>
-            Your Message
-            <textarea
-              name='message'
-              rows='4'
-              className='textarea'
-              placeholder='Write your thoughts here...'
-              value={form.message}
-              onChange={handleChange}
-              onFocus={handleFocus}
-              onBlur={handleBlur}
-            />
-          </label>
+//         <form
+//           ref={formRef}
+//           onSubmit={handleSubmit}
+//           className='w-full flex flex-col gap-7 mt-14'
+//         >
+//           <label className='text-black-500 font-semibold'>
+//             Name
+//             <input
+//               type='text'
+//               name='name'
+//               className='input'
+//               placeholder='John'
+//               required
+//               value={form.name}
+//               onChange={handleChange}
+//               onFocus={handleFocus}
+//               onBlur={handleBlur}
+//             />
+//           </label>
+//           <label className='text-black-500 font-semibold'>
+//             Email
+//             <input
+//               type='email'
+//               name='email'
+//               className='input'
+//               placeholder='John@gmail.com'
+//               required
+//               value={form.email}
+//               onChange={handleChange}
+//               onFocus={handleFocus}
+//               onBlur={handleBlur}
+//             />
+//           </label>
+//           <label className='text-black-500 font-semibold'>
+//             Your Message
+//             <textarea
+//               name='message'
+//               rows='4'
+//               className='textarea'
+//               placeholder='Write your thoughts here...'
+//               value={form.message}
+//               onChange={handleChange}
+//               onFocus={handleFocus}
+//               onBlur={handleBlur}
+//             />
+//           </label>
 
-          <button
-            type='submit'
-            disabled={loading}
-            className='btn'
-            onFocus={handleFocus}
-            onBlur={handleBlur}
-          >
-            {loading ? "Sending..." : "Submit"}
-          </button>
-        </form>
-      </div>
+//           <button
+//             type='submit'
+//             disabled={loading}
+//             className='btn'
+//             onFocus={handleFocus}
+//             onBlur={handleBlur}
+//           >
+//             {loading ? "Sending..." : "Submit"}
+//           </button>
+//         </form>
+//       </div>
 
   
      
-    </section>
-    {/* <Footer /> */}
-    </>
+//     </section>
+//     {/* <Footer /> */}
+//     </>
     
+//   );
+// };
+
+// export default Contact;
+
+
+import React, { useRef } from 'react';
+import emailjs from '@emailjs/browser';
+import './contact.css'
+const ContactForm = () => {
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm(import.meta.env.VITE_APP_EMAILJS_SERVICE_ID, import.meta.env.VITE_APP_EMAILJS_TEMPLATE_ID, form.current, import.meta.env.VITE_APP_EMAILJS_PUBLIC_KEY)
+      .then((result) => {
+          console.log(result.text);
+          alert('Message sent successfully!');
+      }, (error) => {
+          console.log(error.text);
+          alert('An error occurred, please try again.');
+      });
+
+    e.target.reset();
+  };
+
+  return (
+    <form ref={form} onSubmit={sendEmail} className="contact-form">
+      <label>Name</label>
+      <input type="text" name="user_name" required />
+
+      <label>Email</label>
+      <input type="email" name="user_email" required />
+
+      <label>Message</label>
+      <textarea name="message" required />
+
+      <input type="submit" value="Send" />
+    </form>
   );
 };
 
-export default Contact;
+export default ContactForm;
