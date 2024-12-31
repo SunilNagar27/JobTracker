9e53429b03cfb8a67e34214267a134330bad5901
Sunil Nagar <125652150+SunilNagar27@users.noreply.github.com>
2024-12-31 13:50:42 +0530

    Update api.jsx

--- client/src/api.jsx
+++ client/src/api.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://jobtracker-1-e4t8.onrender.com/api",
+  baseURL: "https://jobtracker-3.onrender.com/api",
 });
 
 API.interceptors.request.use((req) => {
