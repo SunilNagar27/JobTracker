9fe2599ecfa51e80ac35cf8d60b5b9256ff2edfa
Sunil Nagar <bewithsunilnagar@gmail.com>
2026-04-21 10:34:27 +0530

    Port update

--- server/server.js
+++ server/server.js
@@ -27,5 +27,8 @@ mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log("MongoDB Connected"))
   .catch((err) => console.log(err));
 
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+const PORT = process.env.PORT;
+
+app.listen(PORT, "0.0.0.0", () => {
+  console.log("Server running on port", PORT);
+});
